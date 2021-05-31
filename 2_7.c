1dff67d3bee9eb579a649b112798ef44bab2343b
JosephLiang <liangzhaoqi1@hotmail.com>
2021-05-31 11:31:49 -0700

    formate update

--- 2_7.c
+++ 2_7.c
@@ -4,10 +4,10 @@
 int max3(int in1, int in2, int in3)
 {
 	int maxout = 0;
-	
+
 	maxout = (in1 > in2) ? in1 : in2;
 	maxout = (in3 > maxout) ? in3 : maxout;
-	
+
 	return maxout;
 }
 
@@ -17,53 +17,53 @@ int maxSubSum(const int a[], int left, int right)
 	int maxLeftBorderSum, maxRightBorderSum;
 	int leftBorderSum, rightBorderSum;
 	int center, i;
-	
-	if(left == right)
+
+	if (left == right)
 	{
-		if(a[left] >0)
+		if (a[left] > 0)
 			return a[left];
 		else
 			return 0;
 	}
-	
+
 	center = (left + right) / 2;
 	maxLeftSum = maxSubSum(a, left, center);
 	maxRightSum = maxSubSum(a, center + 1, right);
-	
+
 	maxLeftBorderSum = 0;
 	leftBorderSum = 0;
-	for(i = center; i >= left; i--)
+	for (i = center; i >= left; i--)
 	{
 		leftBorderSum += a[i];
-		if(maxLeftBorderSum < leftBorderSum)
+		if (maxLeftBorderSum < leftBorderSum)
 			maxLeftBorderSum = leftBorderSum;
 	}
-	
+
 	maxRightBorderSum = 0;
 	rightBorderSum = 0;
-	for(i = center + 1; i <= right; i++)
+	for (i = center + 1; i <= right; i++)
 	{
 		rightBorderSum += a[i];
-		if(maxRightBorderSum < rightBorderSum)
+		if (maxRightBorderSum < rightBorderSum)
 			maxRightBorderSum = rightBorderSum;
 	}
-	
+
 	return max3(maxLeftSum, maxRightSum, maxLeftBorderSum + maxRightBorderSum);
 }
 
 void main()
 {
-	
+
 	int input;
 	int array[] = {3, -5, 1, 5, 10, -12, 32};
 	//int input = getchar() - '0';
 	scanf("%d", &input);
-	if(input > (sizeof(array) / sizeof(array[0])))
+	if (input > (sizeof(array) / sizeof(array[0])))
 	{
 		printf("input %d is larger than the # of elements %d!\n", input, (int)(sizeof(array) / sizeof(array[0])));
 		return;
 	}
 	printf("%d\n", maxSubSum(array, 0, input));
-	
+
 	//printf("%d\n", max3(1, 5, 4));
 }
--- 2_8.c
+++ 2_8.c
@@ -5,15 +5,15 @@
 int maxSubSequenceSum(const int a[], int N)
 {
 	int thisSum, maxSum, i;
-	
+
 	thisSum = maxSum = 0;
-	for(i = 0; i < N; i++)
+	for (i = 0; i < N; i++)
 	{
 		thisSum += a[i];
-		
-		if(thisSum > maxSum)
+
+		if (thisSum > maxSum)
 			maxSum = thisSum;
-		else if(thisSum < 0)
+		else if (thisSum < 0)
 			thisSum = 0;
 	}
 	return maxSum;
@@ -24,27 +24,25 @@ void main(int argc, char *argv[])
 	int input;
 	///////////////1   2  3  4  5   6    7
 	int array[] = {3, -5, 1, 5, 10, -12, 32};
-	
-	int i,j;
-	
-	if(argc != 1)
+
+	int i, j;
+
+	if (argc != 1)
 	{
 		//printf("%s\n", argv[0]); //argv[0] is the name of the program
 		printf("argc: %d\n", argc);
 		printf("argv[1]: %s, size:%d\n", argv[1], (int)strlen(argv[1]));
-		
-		
+
 		//while(*argv[1] != '\0')
 		char *c = argv[1];
 		while (*c != '\0')
 			printf("%c,", *c++);
-		
-		
-		for(i = 0; i < argc; i++)
+
+		for (i = 0; i < argc; i++)
 		{
 			printf("\ndump argv[%d] @ %x:", i, (unsigned int)argv[i]);
-			for(j = 0; j < (strlen(argv[i])); j++)
-				printf("%c,", argv[i][j]);	
+			for (j = 0; j < (strlen(argv[i])); j++)
+				printf("%c,", argv[i][j]);
 		}
 
 		/*
@@ -53,21 +51,21 @@ void main(int argc, char *argv[])
 			printf("%c,", argv[2][i]);
 		printf("\n");	
 		*/
-		
+
 		printf("\n\n\n");
 		input = (int)(argv[1][0] - '0');
 	}
 	else
 	{
-		scanf("%d", &input);	
+		scanf("%d", &input);
 	}
 
-	if(input > (sizeof(array) / sizeof(array[0])))
+	if (input > (sizeof(array) / sizeof(array[0])))
 	{
 		printf("input %d is larger than the # of elements %d!\n", input, (int)(sizeof(array) / sizeof(array[0])));
 		return;
 	}
 	printf("result:%d\n", maxSubSequenceSum(array, input));
-	
+
 	//printf("%d\n", max3(1, 5, 4));
 }
