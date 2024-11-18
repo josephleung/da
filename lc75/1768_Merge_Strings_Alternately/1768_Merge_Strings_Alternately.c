d1d426abbb02f5b88394a2617cd70ffb32ca03f0
Joseph Liang <liangzhaoqi1@hotmail.com>
2024-11-18 10:51:19 -0800

    add my_strcpy for 1768

--- lc75/1768_Merge_Strings_Alternately/1768_Merge_Strings_Alternately.c
+++ lc75/1768_Merge_Strings_Alternately/1768_Merge_Strings_Alternately.c
@@ -26,11 +26,36 @@ char * mergeAlternately(char * word1, char * word2){
     return merged;
 }
 
+char *my_strcpy(char *dest_str, char *src_str)
+{
+    char* dest_start = dest_str;
+    while(*src_str != '\0')
+    {
+        *dest_str = *src_str;
+        dest_str++;
+        src_str++;
+    }
+    *dest_str = '\0';
+
+    printf("\n dest_str %p, %p, *dest_str 0x%x \n", (void*)dest_str, (void*)dest_str, (unsigned int)*dest_str);
+
+    char* temp = dest_start;
+    for(int i=0; i < 10; i++, temp++)
+        printf("*dest_str 0x%x, %c \n", (unsigned int)*temp, *temp);
+
+    return dest_start;
+}
+
 int main()
 {
     char word1[10] = "abc";
     char word2[10] = "pqr";
     char* merged = mergeAlternately(word1, word2);
+    printf("Merged string: %s\n", merged);
+
+    my_strcpy(word1, "ab");
+    my_strcpy(word2, "pqrs");
+    merged = mergeAlternately(word1, word2);
     printf("Merged string: %s\n", merged);
 
     strcpy(word1, "ab");
---
+++ lc75/605_Can_Place_Flowers/605_Can_Place_Flowers.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
+    int count = 0;
+    for (int i = 0; i < flowerbedSize; i++) {
+        if (flowerbed[i] == 0) {
+            int prev = (i == 0) ? 0 : flowerbed[i - 1];
+            int next = (i == flowerbedSize - 1) ? 0 : flowerbed[i + 1];
+            if (prev == 0 && next == 0) {
+                flowerbed[i] = 1;
+                count++;
+                if (count >= n) {
+                    return true;
+                }
+            }
+        }
+    }
+    return count >= n;
+}
+
+int main() {
+    int flowerbed1[] = {1, 0, 0, 0, 1};
+    int flowerbedSize1 = 5;
+    int n1 = 1;
+    printf("Can place flowers: %s\n", canPlaceFlowers(flowerbed1, flowerbedSize1, n1) ? "true" : "false");
+
+    int flowerbed2[] = {1, 0, 0, 0, 1};
+    int flowerbedSize2 = 5;
+    int n2 = 2;
+    printf("Can place flowers: %s\n", canPlaceFlowers(flowerbed2, flowerbedSize2, n2) ? "true" : "false");
+
+    return 0;
+}
