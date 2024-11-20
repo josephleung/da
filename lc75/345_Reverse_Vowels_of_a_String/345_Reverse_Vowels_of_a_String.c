ef5d32dd715914eda9f3ef6813ad0e0e4a59d832
Joseph Liang <liangzhaoqi1@hotmail.com>
2024-11-19 20:08:17 -0800

    add #345

---
+++ lc75/345_Reverse_Vowels_of_a_String/345_Reverse_Vowels_of_a_String.c
@@ -0,0 +1,88 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int is_vowel(char c)
+{
+    if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || //
+        c == 'i' || c == 'I' || c == 'o' || c == 'O' || //
+        c == 'u' || c == 'U')
+        return true;
+    else
+        return false;
+}
+
+#if 1
+char *reverseVowels(char *s)
+{
+    unsigned int str_len = strlen(s);
+    int i = 0;
+    int j = str_len - 1;
+    char temp;
+
+    printf("str_len=%d\n", str_len);
+    for (i = 0, j = str_len - 1; i < j; i++, j--)
+    {
+        printf("i=%d, j=%d\n", i, j);
+        if(is_vowel(s[i]) && is_vowel(s[j]))
+        {
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+        else if(is_vowel(s[i]) && !is_vowel(s[j]))
+        {
+            i--;
+        }
+        else if(!is_vowel(s[i]) && is_vowel(s[j]))
+        {
+            j++;
+        }
+        printf("i=%d, j=%d\n\n", i, j);
+    }
+    return s;
+}
+
+#else
+char *reverseVowels(char *s)
+{
+    int left = 0;
+    int right = strlen(s) - 1;
+    
+    while(left < right)
+    {
+        //find the next vowel from the left
+        while(left < right && !is_vowel(s[left]))
+        {
+            left++;
+        }
+
+        //find the next vowel from the right
+        while(left < right && !is_vowel(s[right]))
+        {
+            right--;
+        }
+
+        //swap the vowel
+        if(left < right)
+        {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    return s;
+}
+
+#endif
+
+int main()
+{
+    char test[] = "IceCreAm";  //AceCreIm
+    printf("%s\n", test);
+    printf("%s\n", reverseVowels(test));
+    return 0;
+}
