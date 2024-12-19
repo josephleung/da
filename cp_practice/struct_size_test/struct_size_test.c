809b456e4fbe9098c8a4df8ba607dcf23da41e88
Joseph Liang <liangzhaoqi1@hotmail.com>
2024-12-18 21:54:44 -0800

    update struct size test

--- cp_practice/struct_size_test/struct_size_test.c
+++ cp_practice/struct_size_test/struct_size_test.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+
 
 // https://stackoverflow.com/questions/3318410/pragma-pack-effect
-#pragma pack(1)
+// #pragma pack(1)
 typedef struct free_block
 {
     char index;
-    char marker_0;
-    int a;
-    int *b;
-    char *data;
-    char marker_1;
+    // char marker_0;
+    // int a;
+    // int *b;
+    // char *data;
+    // char marker_1;
     struct free_block *next;
-    char marker_2;
+    // char marker_2;
 } free_block_t;
-#pragma pack()
+// #pragma pack()
+
+typedef struct
+{
+    uint16_t count;
+    uint16_t data[2];
+    uint32_t timestamp;
+} packet_S;
 
 int main()
 {
@@ -27,5 +36,18 @@ int main()
 
     printf("sizeof(free_block_t):%d\n", (int)sizeof(free_block_t));
 
+
+    printf("sizeof(packet_S):%d\n", (int)sizeof(packet_S));
+
+    unsigned int x = 0xaabbccdd;
+    char *c = (char*)&x;
+    if (*c)
+    {
+        printf("*c: %x, Machine is Little Endian\n", *c);
+    }
+    else
+    {
+        printf("Machine is Big Endian\n");
+    }
     return 0;
 }
